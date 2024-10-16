a7b5549b4e9f501160410b80cf7d9a5ac10cafa4
evosrizal <evosrizal158@gmail.com>
2024-10-16 21:12:10 +0700

    isi controller perangkat

---
+++ app/Http/Controllers/API/AbsensiController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Perangkat;
+use Illuminate\Http\Request;
+
+class AbsensiController extends Controller
+
+{
+    public function index()
+    {
+        $perangkats = Perangkat::all();
+        return response()->json($perangkats);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'jabatan' => 'required',
+            'nip' => 'required|unique:perangkats'
+        ]);
+
+        $perangkat = Perangkat::create($request->all());
+        return response()->json($perangkat, 201);
+    }
+
+    public function show($id)
+    {
+        $perangkat = Perangkat::findOrFail($id);
+        return response()->json($perangkat);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $perangkat = Perangkat::findOrFail($id);
+        $perangkat->update($request->all());
+        return response()->json($perangkat);
+    }
+
+    public function destroy($id)
+    {
+        $perangkat = Perangkat::findOrFail($id);
+        $perangkat->delete();
+        return response()->json(null, 204);
+    }
+}
